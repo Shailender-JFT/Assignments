327eb37fc8db371d171ac57c80e3d06dca86c57e
Shailender-JFT <shailender.yadav@jellyfishtechnologies.com>
2022-07-16 12:45:03 +0530

    swap

---
+++ swap.js
@@ -0,0 +1,17 @@
+var a= 12;
+var b= 23;
+
+console.log("before a "+a);
+console.log("before b "+b);
+
+
+a=a+b;
+console.log(a);
+
+b=a-b;
+console.log(b);
+
+a=a-b;
+
+console.log("after a "+a);
+console.log("after b "+b);
